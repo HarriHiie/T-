f0775205ab45860385a333de0909792a986e3e97
HarriHiie <89829583+HarriHiie@users.noreply.github.com>
2023-02-21 09:12:30 +0200

    Add files via upload

---
+++ Töö.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var schools = new Dictionary<string, string>();
+
+            schools.Add("TTHK", "");
+            schools.Add("TKG", "");
+            schools.Add("POLÜ", "");
+            
+
+            foreach (var school in schools)
+            {
+                Console.WriteLine($"{school.Key}");
+            }
+           
+
+        }
+    }
+}
